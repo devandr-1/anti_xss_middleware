8d7f9d5c74bb384c8f3257b559028f74559a9c1c
andrew <andrew@dev.com>
2021-10-13 10:08:04 +0300

    Init

---
+++ WebApplication/ViewModels/SomeViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebApplication.ViewModels
+{
+    public class SomeViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int? TypeId { get; set; }
+    }
+}
